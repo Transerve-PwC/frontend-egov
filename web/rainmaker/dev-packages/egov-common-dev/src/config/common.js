3bf9eb853544e8843c6346d31527bc6162bed5a9
Tejaswi Katha <tejaswi@webileapps.com>
2021-01-19 11:33:52 +0530

    changed the map api key (got from nirav)

--- web/rainmaker/dev-packages/egov-common-dev/src/config/common.js
+++ web/rainmaker/dev-packages/egov-common-dev/src/config/common.js
@@ -1,5 +1,5 @@
 const commonConfig = {
-  MAP_API_KEY: "AIzaSyCH9PmCbk_mcpgijAAlTeltC4deOxC5wEM",
+  MAP_API_KEY: "AIzaSyAX8q2e4242YxR9JMBewQ6bABrywYv8btE",
   tenantId: process.env.REACT_APP_DEFAULT_TENANT_ID
   // forgotPasswordTenant: "pb.amritsar",
 };
--- web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-config/app-config.js
+++ web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-config/app-config.js
@@ -1,2 +1,2 @@
 export const defaultLocation = { lat: 30.7333, lng: 76.7794 };
-export const MAP_API_KEY = "AIzaSyCH9PmCbk_mcpgijAAlTeltC4deOxC5wEM";
+export const MAP_API_KEY = "AIzaSyAX8q2e4242YxR9JMBewQ6bABrywYv8btE";
